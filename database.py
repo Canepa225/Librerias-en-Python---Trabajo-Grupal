7ed6bd2df087c0c6e377a50a931a07251daa74fc
Canepa225 <canepa.igna@gmail.com>
2022-06-03 21:19:43 -0300

    primer commit - conexión con database

---
+++ database.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+CREATE_BEANS_TABLE = "CREATE TABLE IF NOT EXISTS beans (id INTEGER PRIMARY KEY, name TEXT, method TEXT, rating INTEGER);"
+
+INSERT_BEAN = "INSERT INTO beans (name, method, rating) VALUES (?, ?, ?);"
+
+GET_ALL_BEANS = "SELECT * FROM beans;"
+GET_BEANS_BY_NAME = "SELECT * FROM beans WHERE name = ?;"
+GET_BEST_PREPARATION_FOR_BEAN = """
+SELECT * FROM beans
+WHERE name = ?
+ORDER BY rating DESC
+LIMIT 1; """
+
+def connect():
+    return sqlite3.connect("database.db")
