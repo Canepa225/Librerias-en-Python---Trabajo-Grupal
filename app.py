ea4a0efc780ceb6f836ef6627427086fac31286c
TomiGenari <tomasgenari@gmail.com>
2022-06-03 22:41:36 -0300

    creacion de app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+import database
+
+MENU_PROMPT = """
+
+==============================
+    --Registro de cafés--
+
+Eliga una opcion:
+
+1) Agregar un café.
+2) Ver todos los cafés.
+3) Encontrar café por nombre.
+4) Ver mejor método de preparación de un grano de café.
+5) Salir.
+
+Tu selección: """
+
+
