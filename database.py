28cf2f7f8f7e165a3f1d1bd427167e41514f8212
TomiGenari <tomasgenari@gmail.com>
2022-06-03 22:22:55 -0300

    Agregado d funciones database.py

--- database.py
+++ database.py
@@ -16,3 +16,25 @@ def connect():
     return sqlite3.connect("database.db")
 
 print('ok')
+
+
+def create_tables(connection):
+    with connection:
+        connection.execute(CREATE_BEANS_TABLE)
+
+
+def add_bean(connection, name, method, rating):
+    with connection:
+        connection.execute(INSERT_BEAN, (name, method, rating))
+
+def get_all_beans(connection):
+    with connection:
+        return connection.execute(GET_ALL_BEANS).fetchall()
+
+def get_beans_by_name(connection, name):
+    with connection:
+        return connection.execute(GET_BEANS_BY_NAME, (name,)).fetchall()
+
+def get_best_preparation_for_bean(connection, name):
+    with connection:
+        return connection.execute(GET_BEST_PREPARATION_FOR_BEAN, (name,)).fetchone()
